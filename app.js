958b1fbb3331c9638d533c385b96333c3d6cc184
lago-pete <lagonegropeter@gmail.com>
2025-01-25 13:00:03 -0800

    stopping

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finish the Course and Learn Vue!',
+            vueLink: 'https://vuejs.org/guide/quick-start'
+        };
+    }
+
+
+});
+
+
+
+app.mount('#user-goal');
