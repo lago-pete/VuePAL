e84b8491a5fdd569bbb321e355918d8c6d3d1663
lago-pete <lagonegropeter@gmail.com>
2025-01-28 21:58:00 -0800

    update

--- app.js
+++ app.js
@@ -4,11 +4,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the Course and Learn Vue!',
+            courseGoalA: 'Finish the Course and Learn Vue!',
+            courseGoalB: '<h2>Master Vue and build amazing projects!</h2>',
             vueLink: 'https://vuejs.org/guide/quick-start'
         };
+    },
+    methods:{
+        outputGoal(){
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5){
+                return this.courseGoalA;
+            }
+            else{
+                return this.courseGoalB;
+            }
+        }
     }
-
 
 });
 
