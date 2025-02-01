0c2cc81fe030f4f69162b79d00a5c9f6c7b69125
lago-pete <lagonegropeter@gmail.com>
2025-02-01 10:43:16 -0800

    restart

---
+++ vue_pal/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Hello, Vue!</h2>
+    <p>Welcome to your first Vue app.</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+<style scoped>
+h2 {
+  color: blue;
+}
+</style>
